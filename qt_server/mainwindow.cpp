f76f6c98946840ddd768a8d4bf5872ae82e9f1dd
wen2ms <1819509111@qq.com>
2025-02-25 21:44:39 +0800

    Feat: implement the server communication

--- qt_server/mainwindow.cpp
+++ qt_server/mainwindow.cpp
@@ -4,8 +4,38 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    
+    ui->port->setText("9999");
+    
+    server_ = new QTcpServer(this);
+    
+    connect(server_, &QTcpServer::newConnection, this, [=]() {
+        socket_ = server_->nextPendingConnection();
+        
+        connect(socket_, &QTcpSocket::readyRead, this, [=]() {
+            QByteArray data = socket_->readAll();
+            
+            ui->record->append("Client says: " + data);
+        });
+    });
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_set_listen_clicked() {
+    unsigned short port = ui->port->text().toUShort();
+    
+    server_->listen(QHostAddress::Any, port);
+    
+    ui->set_listen->setDisabled(true);
+}
+
+void MainWindow::on_send_message_clicked() {
+    QString message = ui->message->toPlainText();
+    
+    socket_->write(message.toUtf8());
+    
+    ui->message->append("Server says: " + message);
 }
--- qt_server/mainwindow.h
+++ qt_server/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -15,8 +17,16 @@ class MainWindow : public QMainWindow {
   public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+    
+  private slots:    
+    void on_set_listen_clicked();
+    
+    void on_send_message_clicked();
+    
   private:
     Ui::MainWindow *ui;
+    
+    QTcpServer* server_;
+    QTcpSocket* socket_;
 };
 #endif  // MAINWINDOW_H
